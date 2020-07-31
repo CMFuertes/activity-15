429e22e5bba4d58ec4b1b111485e418b8a12cf22
Cristhal Fuertes <cristhal.fuertes@gmail.com>
2020-07-31 18:52:28 -0400

    Adding file append

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 const inquirer = require("inquirer");
 
-const inquirer = require('inquirer');
-
 const fs = require('fs');
+
+inquirer.prompt([
+{
+    type: "input",
+    message: "What is your name?",
+    name: "username"
+},
+{
+    type: "checkbox",
+    message: "What languages do you know?",
+    choices: ["English", "French", "Japanese", "Russian", "Bulgarian",],
+    name: "languages"
+},
+{
+    type: "list",
+    message: "What is your preferred method of communciation?",
+    choices: ["Klingon", "Fax", "Messenger Pigeon", "Hiccups"],
+    name: "communication",
+}
+  ])
+  .then(function(response) {
+      console.log(response);
+      fs.appendFile("log.text", JSON.stringify(response, null, 2)+ "\n", 'utf8', function(err, data){
+        if(err) return console.log(err);
+        console.log("written to log.txt");
+      });
+
+  });
